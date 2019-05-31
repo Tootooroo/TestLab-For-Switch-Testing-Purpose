392dbb8bafdf6f7f122190b82e85af524bd0e5a6
Tootooroo <aydenlin@foxmail.com>
2019-05-31 21:07:49 +0800

    scriptDB add

---
+++ include/scriptDB.h
@@ -0,0 +1,15 @@
+/* scriptDB.h */
+
+#ifndef _SCRIPT_DB_H_
+#define _SCRIPT_DB_H_
+
+#include <dirent.h>
+
+typedef struct scriptDB {
+    DIR *dbDir;
+    int numOfScripts;
+    /* Path to database */
+    char *path;
+} scriptDB;
+
+#endif /* _SCRIPT_DB_H_ */
